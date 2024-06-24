0b1d77622d26617ef3dac01c221191a10a8c82bb
Mihai Nadăș <mihai@nadas.ro>
2024-06-25 00:21:11 +0300

    introduced count_duplicates.py

---
+++ count_duplicates.py
@@ -0,0 +1,19 @@
+import argparse
+
+def count_duplicates(text):
+    return (len(text), len(text) - len(set(text)))
+
+def main():
+    parser = argparse.ArgumentParser(description="Counts the duplicate lines in a file passed as argument.")
+    parser.add_argument(
+        "--input",
+        type=str,
+        required=True
+    )
+    args = parser.parse_args()
+    with open(args.input) as f:
+        input_len, input_duplicates = count_duplicates(f.readlines())
+        print(f"{args.input} contains {input_len} lines, out of which {input_duplicates} are duplicate.")
+
+if __name__ == "__main__":
+    main()
